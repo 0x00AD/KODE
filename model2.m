3fb5f9fe605ef08a1088bfc62cd88ac4c0bba162
Perttu Yli-Opas <perttuyo@gmail.com>
2018-02-23 18:27:21 +0300

    Simulation updates at Porto

--- model2.m
+++ model2.m
@@ -3,14 +3,16 @@
 
 % Name the variables
 
-m_patient = 30; % kg
-m_bed = 10; % kg
+m_patient = 40; % kg
+m_bed = 8; % kg
 m_payload = m_bed + m_patient; % kg
+
+feet_delay = 0.1; % s
 
 max_offset = 0.1; % m
 
 k_spring = 2500; % N/m
-damp_spring = 0; % whatever
+damp_spring = 1; % whatever
 
 k_cutoffspring = 20000; % N/m
 
@@ -18,7 +20,7 @@ linear_act_speed = 0.05; % m/s
 linear_act_max = 0.5; % m
 linear_act_delay = 0.1; % s
 
-DC_max_force = 200; % N
+DC_max_force = 800; % N
 DC_delay = 0.005; % s
 
 noise_power = 0.01;
@@ -30,42 +32,62 @@ noise_start_time = 3; % s
 patient_sit_time = 0.5; % s
 patient_sit_pace = 50; % kg/s
 
-DC_PID_P = 0;
-DC_PID_I = 0;
-DC_PID_D = 1000;
-DC_PID_N = 100;
 
-sim('model2_sim')
+P = 0;
+I = 0;
+D = 200;
+N = 100;
 
-max_bed_a = sort(findpeaks(abs(bed_acc.data)));
-acc_peaks = max_bed_a(end-10:end)
-max_plat_a = max(abs(platform_acc.data))
+reduction = runsim(P, I, D, N)
 
-figure
-hold on
-plot(bed_pos_abs + 0.2)
-plot(platform_pos)
-plot(lin_act_pos)
-plot(pos_reset)
-legend('bed pos', 'platform', 'lin act pos', 'RESET', 'location', 'best')
-title('With actual position')
 
-figure
-hold on
-plot(bed_pos_abs - platform_pos + 0.2)
-plot(platform_pos)
-plot(lin_act_pos)
-plot(pos_reset)
-legend('bed pos', 'platform', 'lin act pos', 'RESET', 'location', 'best')
-title('With relative position')
+% figure
+% plot(bed_angle)
+% title('Bed angle')
 
-figure
-hold on
-plot(DC_force)
-plot(spring_force)
-plot(-1 * payload_weight)
-plot(bed_force)
-plot(cutoff_force)
-legend('DC force', 'spring force', 'bed + patient weight', 'total force to bed', 'cutoff force', 'location', 'best')
+% figure
+% hold on
+% plot(bed_pos_abs + 0.2)
+% plot(platform_pos)
+% plot(lin_act_pos)
+% plot(pos_reset)
+% legend('Head pos', 'platform', 'lin act pos', 'RESET', 'location', 'best')
+% title('Head, actual position')
+% 
+% % figure
+% % hold on
+% % plot(bed_pos_abs2 + 0.2)
+% % plot(platform_pos2)
+% % plot(lin_act_pos2)
+% % plot(pos_reset2)
+% % legend('Feet pos', 'platform', 'lin act pos', 'RESET', 'location', 'best')
+% % title('Feet, actual position')
+% 
+% figure
+% hold on
+% plot(bed_pos_abs - platform_pos + 0.2)
+% plot(platform_pos)
+% plot(lin_act_pos)
+% plot(pos_reset)
+% legend('bed pos', 'platform', 'lin act pos', 'RESET', 'location', 'best')
+% title('Head, relative position')
+% 
+% % figure
+% % hold on
+% % plot(bed_pos_abs2 - platform_pos2 + 0.2)
+% % plot(platform_pos2)
+% % plot(lin_act_pos2)
+% % plot(pos_reset2)
+% % legend('bed pos', 'platform', 'lin act pos', 'RESET', 'location', 'best')
+% % title('Feet, relative position')
+% 
+% figure
+% hold on
+% plot(DC_force)
+% plot(spring_force)
+% plot(-1 * payload_weight)
+% plot(bed_force)
+% plot(cutoff_force)
+% legend('DC force', 'spring force', 'bed + patient weight', 'total force to bed', 'cutoff force', 'location', 'best')
 
 
