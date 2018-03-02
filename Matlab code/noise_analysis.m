77494563fabdb0d4c69cb212eb08de4c3d77f39f
Perttu Yli-Opas <perttuyo@gmail.com>
2018-03-02 23:05:46 +0200

    Added todos :P

---
+++ Matlab code/noise_analysis.m
@@ -0,0 +1,5 @@
+% TODO!!!
+
+% Make Markov chain, analyse the transfer probabilities for each pair
+% Make the probabilities symmetric, effectively eliminating tilt errors
+% Use the transfer probabilities to generate new data
